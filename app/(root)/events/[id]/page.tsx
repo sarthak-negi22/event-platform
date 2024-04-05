6de5d7dff620da45b5511c6077adc23e7436d1d6
Sarthak <saryatnam22@gmail.com>
2024-04-05 16:03:03 +0530

    testing stripe payment functionality

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,8 +1,10 @@
+import CheckoutButton from "@/components/shared/CheckoutButton";
 import Collection from "@/components/shared/Collection";
 import { getEventById, getRelatedEventsByCategory } from "@/lib/actions/event.actions";
 import { formatDateTime } from "@/lib/utils";
 import { SearchParamProps } from "@/types";
 import Image from "next/image";
+import Link from "next/link";
 
 const EventDetails = async ({ params : { id } , searchParams} : SearchParamProps) => {
     const event = await getEventById(id);
@@ -28,7 +30,7 @@ const EventDetails = async ({ params : { id } , searchParams} : SearchParamProps
                     <h2 className = "h2-bold">{ event.title }</h2>
                     <div className = "flex flex-col gap-3 sm:flex-row sm:items-center">
                         <div className = "flex gap-3">
-                            <p className = "p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700">{ event.isFree ? 'FREE' : `$${event.price}`}
+                            <p className = "p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700">{ event.isFree ? 'FREE' : `₹${event.price}`}
                             </p>
                             <p className = "p-medium-16 rounded-full bg-grey-500/10 px-4 py-2.5 text-grey-500">
                                 { event.category.name }
@@ -40,7 +42,7 @@ const EventDetails = async ({ params : { id } , searchParams} : SearchParamProps
                         </p>
                     </div>
                 </div>
-                {/* CHECKOUT BUTTON */}
+                <CheckoutButton event = { event } />
 
                 <div className = "flex flex-col gap-5">
                     <div className = "flex gap-2 md:gap-3 ">
@@ -70,7 +72,9 @@ const EventDetails = async ({ params : { id } , searchParams} : SearchParamProps
                 <div className = "flex flex-col gap-2">
                     <p className = "p-bold-20 text-grey-600">What You'll Learn:</p>
                     <p className = "p-medium-16 lg:p-regular-18">{ event.description }</p>
-                    <p className = "p-medium-16 lg:p-regular-18 truncate text-primary-500 underline">{ event.url }</p>
+                    <Link href = {`${event.url}`}>
+                        <p className = "p-medium-16 lg:p-regular-18 truncate text-primary-500 underline" >{ event.url }</p>
+                    </Link>
 
                 </div>
             </div>
@@ -88,8 +92,6 @@ const EventDetails = async ({ params : { id } , searchParams} : SearchParamProps
             page = { 1 }
             totalPages = { 2 }
           />
-
-
     </section>
     </>
   )
