fbf96cd8e81f430278926bd4689223cf7d46f2d4
Sarthak <saryatnam22@gmail.com>
2024-04-05 16:14:30 +0530

    changed inr to usd in stripe

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -240,7 +240,7 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
                     <FormControl>
                         <div className = "flex-center h-[54px] w-full overflow-hidden rounded-full bg-gray-50 px-4 py-2">
                             <Image 
-                                src = "/assets/icons/rupee.svg.png"
+                                src = "/assets/icons/dollar.svg"
                                 alt = "dollar"
                                 width = { 24 }
                                 height = { 24 }
