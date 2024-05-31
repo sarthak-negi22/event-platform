f6fff1ecf212332baa4db815573fafe7b5e4af8f
Sarthak <saryatnam22@gmail.com>
2024-05-31 18:18:59 +0530

    changed root page heading

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -32,7 +32,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
           <div className = "wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
             <div className = "flex flex-col justify-center gap-8">
               <h1 className = "h1-bold">
-                Host, Connect, Celebrate: Your Events, Out Platform!
+                Host, Connect, Celebrate: Your Events, Our Platform!
               </h1>
                   <p className = "p-regular-20 md:p-regular-24">
                     Book and learn helpful tips from numerous mentors in world-class companies with out global community.
