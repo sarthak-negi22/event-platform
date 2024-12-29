bfb8179795d7f526ef8b9ccb59743e9659feaa87
Sarthak <saryatnam22@gmail.com>
2024-12-29 11:50:30 +0530

    token test

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -11,7 +11,7 @@ export const metadata : Metadata = {
   title : 'Evently | Event Overview'
 }
 
-
+// test 
 const EventDetails = async ({ params : { id } , searchParams} : SearchParamProps) => {
     const event = await getEventById(id);
     const relatedEvents = await getRelatedEventsByCategory({
